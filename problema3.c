43a6fc8608f8456b29f12bc075107b7760d29f2a
Pabvalb <pabvalb02@gmail.com>
2026-04-27 21:37:33 -0600

    Create problema3.c

---
+++ problema3.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    float dolares, peso;
+    float dolares cambio = 11.96;
+    printf("Ingrese la cantidad de dolares: ");
+    scanf("%f, &dolares);
+    pesos= dolares * dolares cambio;
+    printf("la cantidad en pesos es: %.2f\n", pesos);
+    return 0;
+}
